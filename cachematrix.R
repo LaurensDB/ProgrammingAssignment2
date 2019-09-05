a8ef39ca566d1d0c1ebd717a3381ea66dad88a59
LaurensDB <54947924+LaurensDB@users.noreply.github.com>
2019-09-05 18:05:01 +0200

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This pair of functions will cache the inverse of a given matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+        
+makeCacheMatrix <- function(x = matrix()) {             ## function with argument x defined as matrix
+                
+        inv <- NULL                                     ## the inverse matrix starts with NULL value 
+        set <- function(y) {                            ## define the set function  
+                        
+                x <<- y                                 ## value of matrix in parent environment
+                inv <<- NULL                            ## if there is a new matrix, reset the inverse matrix to NULL
+                        
+        }
+                
+        get <- function() x                             ## define the get fucntion, this returns value of the matrix
+                
+        setinverse <- function(inverse) inv <<- inverse ## sets the value of inverse matrix in parent environment
+        getinverse <- function() inv                    ## gets the value of inverse matrix
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
+        
 }
+        
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinverse()                            
+        if(!is.null(inv)) {                             ## if there is already a value assigned for the inverse matrix,
+                                                        
+                message("getting cached data")          ## a message is returned, 
+                return(inv)                             ## along with the inverse matrix
+                
+        }
+        
+        data <- x$get()                                 ## get the submitted matrix
+        inv <- solve(data, ...)                         ## inverse the matrix
+        x$setinverse(inv)                               ## set the inverse matrix
+        inv                                             ## return the inverse matrix
+        
 }
+
+
+#Thank you for grading my assignment - Laurens
+
+
+
+
